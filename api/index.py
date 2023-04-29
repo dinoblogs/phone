f97394c14a46db8cac514f81620243c3fff268fe
dinoblogs <113527802+dinoblogs@users.noreply.github.com>
2023-04-29 18:06:23 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -4,7 +4,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'My Name Is Kanha Sharma'
+    return '''
+    <html>
+    <head>
+    <title>Krishna Website</title>
+    
+    </head>
+    <body>
+    <h1>Testing123...</h1>
+    </body>
+    
+    </html>
+    
+    '''
 
 @app.route('/about')
 def about():
